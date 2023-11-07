fbacc2f2069b8a0b9f2514d2a494a6ca2651ab45
iren <irasibil2@gmail.com>
2023-11-07 17:12:43 +0200

    feature: implemented get all user's posts
    added dependencies

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -7,9 +7,17 @@ use Illuminate\Http\Request;
 
 class PostsController extends Controller
 {
-    public function index()
+    public function index(Request $request)
     {
-        $posts = Post::orderBy('created_at', 'ASC')->simplePaginate(9);
+        $userPosts = $request->input('user');
+
+        $postsQuery = Post::orderBy('created_at', 'ASC');
+
+        if (isset($userPosts)) {
+            $postsQuery->where('author_id', auth()->id());
+        }
+
+        $posts = $postsQuery->simplePaginate(9);
 
         return response()->json($posts);
     }
--- app/Models/Post.php
+++ app/Models/Post.php
@@ -4,10 +4,21 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Post extends Model
 {
     use HasFactory;
 
     public $timestapms = true;
+
+    /**
+     * Get the user that owns the Post
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class, 'author_id');
+    }
 }
