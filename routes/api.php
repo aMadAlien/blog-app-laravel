6e42cfca8c9752f41c2f76baef11395679c64dfb
iren <irasibil2@gmail.com>
2023-11-08 12:51:31 +0200

    fix: replace controllers into "api" namespace

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,8 @@
 <?php
 
-use App\Http\Controllers\AuthController;
-use App\Http\Controllers\UserController;
-use App\Http\Controllers\PostsController;
+use App\Http\Controllers\Api\AuthController;
+use App\Http\Controllers\Api\UserController;
+use App\Http\Controllers\Api\PostsController;
 use Illuminate\Support\Facades\Route;
 
 /*
